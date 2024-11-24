24807e4c2b661c14527c2cc8e5fcef565ae18787
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2024-11-24 13:36:17 -0300

    fix/front-office: tailwindcss config from design-system package

--- apps/front-office/tailwind.config.ts
+++ apps/front-office/tailwind.config.ts
@@ -1,18 +1,3 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+/* eslint-disable @typescript-eslint/no-require-imports */
+const designSystemConfig = require("design-system/tailwind.config");
+export default designSystemConfig;
