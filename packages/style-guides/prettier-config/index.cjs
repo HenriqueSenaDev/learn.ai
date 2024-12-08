2311616796c0a156c0aca70bba2568bec9c65434
HenriqueSenaDev <acopsenadeveloper@gmail.com1>
2024-12-08 09:45:31 -0300

    style-guides/feat: prettier-configuration

---
+++ packages/style-guides/prettier-config/index.cjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSameLine: false,
+}
+
+module.exports = config;
